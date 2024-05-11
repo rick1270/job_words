3d787ed7a43bb3e75e8a8acc6770c0bcb21b9ff8
Rick Clayton <rick1270@gmail.com>
2024-05-11 19:45:14 -0400

    aws

--- __main__.py
+++ __main__.py
@@ -5,7 +5,6 @@ import json
 import os
 import ids
 import words
-import snot
 
 now = int(time.time() * 1000000)
 FILE_NAME = "variables.json"
@@ -16,13 +15,12 @@ keyword = variable["variables"]["keyword"]  # one variable
 location = variable["variables"]["location"]  # list
 api_key = os.getenv("API_KEY")  # one variable
 days = variable["variables"]["days"]  # one variable
-folder = "Data"  # one variable
-TABLE_NAME = "PC_DBT_DB.RAW.JOB_WORDS_RAW"  # one variable
+FOLDER = "Data"  # one variable
+TABLE_NAME = "JOB_WORDS_RAW"  # one variable
 
 for l in location:
-    ids.just_ids(keyword, l, api_key, days, folder)
+    ids.just_ids(keyword, l, api_key, days, FOLDER)
 
-df_new = words.come_together(path=folder, table_name=TABLE_NAME)
+df_new = words.come_together(path=FOLDER, table_name=TABLE_NAME)
 df_new.to_pickle(f"Backups/Backup{now}")
 print(df_new)
-snot.df_to_table(df_new, TABLE_NAME)
